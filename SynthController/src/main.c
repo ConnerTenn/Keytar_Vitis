272701768d685e763031e9b69dee95dbd744c9b3
Conner <connertenn@gmail.com>
2020-07-13 01:58:05 -0400

    Updated main.c
    Styling overhaul

--- SynthController/src/main.c
+++ SynthController/src/main.c
@@ -19,73 +19,48 @@
 #include "xuartps.h"
 #include "xil_printf.h"
 
-/*
- * The following constants map to the XPAR parameters created in the
- * xparameters.h file. They are defined here such that a user can easily
- * change all the needed parameters in one place.
- */
-#define UART_DEVICE_ID                  XPAR_XUARTPS_0_DEVICE_ID
 
+#define UART_DEVICE_ID  XPAR_XUARTPS_0_DEVICE_ID
 
-int UartPsHelloWorldExample(u16 DeviceId);
-
-
-
-int main(void)
+int InitUart(u16 DeviceId)
 {
-	int Status;
+    int status;
 
-	/*
-	 * Run the Hello World example , specify the the Device ID that is
-	 * generated in xparameters.h
-	 */
-	Status = UartPsHelloWorldExample(UART_DEVICE_ID);
+    //Initialize the UART driver so that it's ready to use
+    //Look up the configuration in the config table and then initialize it.
+    XUartPs_Config *Config = XUartPs_LookupConfig(DeviceId);
+    if (Config == NULL) { return XST_FAILURE; }
+    
+    XUartPs Uart_Ps; //The instance of the UART Driver
+    status = XUartPs_CfgInitialize(&Uart_Ps, Config, Config->BaseAddress);
+    if (status != XST_SUCCESS) { return XST_FAILURE; }
 
-	if (Status == XST_FAILURE) {
-		xil_printf("Uartps hello world Example Failed\r\n");
-		return XST_FAILURE;
-	}
+    XUartPs_SetBaudRate(&Uart_Ps, 115200);
+    if (status != XST_SUCCESS) { return XST_FAILURE; }
 
-	xil_printf("Successfully ran Uartps hello world Example\r\n");
-
-	while (1)
-	{
-		xil_printf("Hello World!!!\n");
-	}
-
-	return Status;
+    return XST_SUCCESS;
 }
 
 
-XUartPs Uart_Ps;		/* The instance of the UART Driver */
-int UartPsHelloWorldExample(u16 DeviceId)
+int main()
 {
-	u8 HelloWorld[] = "Hello World";
-	int SentCount = 0;
-	int Status;
-	XUartPs_Config *Config;
+    int status;
+    status = InitUart(UART_DEVICE_ID);
 
-	/*
-	 * Initialize the UART driver so that it's ready to use
-	 * Look up the configuration in the config table and then initialize it.
-	 */
-	Config = XUartPs_LookupConfig(DeviceId);
-	if (NULL == Config) {
-		return XST_FAILURE;
-	}
+    if (status != XST_SUCCESS) 
+    {
+        xil_printf("Uartps hello world Example Failed\r\n");
+        return XST_FAILURE;
+    }
 
-	Status = XUartPs_CfgInitialize(&Uart_Ps, Config, Config->BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
 
-	XUartPs_SetBaudRate(&Uart_Ps, 115200);
+    uint32_t count = 0;
+    while (1)
+    {
+        xil_printf("Hello World! %lu\n", count++);
+    }
 
-	while (SentCount < (sizeof(HelloWorld) - 1)) {
-		/* Transmit the data */
-		SentCount += XUartPs_Send(&Uart_Ps,
-					   &HelloWorld[SentCount], 1);
-	}
-
-	return SentCount;
+    return XST_SUCCESS;
 }
+
+
