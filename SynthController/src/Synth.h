70df1bc8c83ed053845a18bbd88cb54a329f5d78
Conner <connertenn@gmail.com>
2020-07-25 03:52:24 -0400

    Refactored bus addresses

--- SynthController/src/Synth.h
+++ SynthController/src/Synth.h
@@ -1,6 +1,6 @@
 
 
-#define SYNTH_ADDR (0x40000000)
+#define SYNTH_ADDR (XPAR_APBSLAVE_BREAKOUT_SYNTH_BASEADDR)
 #define SYNTH_ACCESS_RAW() (*(uint32_t *)(SYNTH_ADDR))
 #define SYNTH_ACCESS(reg) (*(uint32_t *)(SYNTH_ADDR + 4*(reg)))
 
